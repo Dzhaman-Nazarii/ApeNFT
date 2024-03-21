b4c17ded95cf2858b60b958227f38f63bfd86836
Nazarii <dzhaman.nazar2003@gmail.com>
2024-03-21 18:35:58 +0200

    add scg and start do header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,30 @@
 import css from './Header.module.css'
 
+import madeByApesLogo from '../../images/icons/madeByApes.svg'
+import discordLogo from '../../images/icons/discordLogo.svg'
+import dockerLogo from '../../images/icons/dockerLogo.svg'
+import spacexLogo from '../../images/icons/spacexLogo.svg'
+
 const Header = () => {
-    return <h1 className={css.header}>Header</h1>
+    return <header>
+            <a href="/">
+                <img className={css.header_apes_icon} src={madeByApesLogo} alt='madeByApesLogo' />
+            </a>
+            <ul className={css.header_list}>
+                <li className={css.header_item}>
+                    <button className={css.header_menu}>Menu</button>
+                </li>
+                <li className={css.header_item}>
+                    <img className={css.header_social_icon} src={discordLogo} alt='discordLogo' />
+                </li>
+                <li className={css.header_item}>
+                    <img className={css.header_social_icon} src={dockerLogo} alt='dockerLogo' />
+                </li>
+                <li className={css.header_item}>
+                    <img className={css.header_social_icon} src={spacexLogo} alt='spacexLogo' />
+                </li>
+            </ul>
+    </header>
 }
 
 export default Header
